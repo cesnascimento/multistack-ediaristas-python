cfda7b4a6db7bd516840ffded361e82ce1aa4055
Cesar Nascimento <cnascimento@dermage.com.br>
2023-08-28 16:21:29 -0300

    Módulo de autenticação finalizada

--- ediaristas/settings.py
+++ ediaristas/settings.py
@@ -125,7 +125,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = 'listar_servicos'
+
 LOGOUT_REDIRECT_URL = 'logar_usuario'
+
+LOGIN_URL = 'logar_usuario'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
